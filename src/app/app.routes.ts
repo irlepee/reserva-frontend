22eddaee55149c6e7e9e4e9cb7fe1bdf0ab41e4a
AlexIA075 <163032623+irlepee@users.noreply.github.com>
2025-11-18 21:26:49 -0700

    Día 1 - Estructura, home, navbar, footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { DashboardHome } from './pages/dashboard/dashboard-home/dashboard-home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component:Home },
+    { path: 'about', component:About},
+    { path: 'dashboard', component:DashboardHome},
+    { path: '***', redirectTo: ''}
+];
---
+++ src/app/pages/dashboard/sites/sites.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sites',
+  imports: [],
+  templateUrl: './sites.html',
+  styleUrl: './sites.css',
+})
+export class Sites {
+
+}
