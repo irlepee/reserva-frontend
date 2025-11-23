cb04bab3a136763b1a8f0adee9b546f08f1f4ffe
AlexIA075 <163032623+irlepee@users.noreply.github.com>
2025-11-23 02:03:19 -0700

    BACKEND CONECTADO - Login funciona

---
+++ src/app/core/services/authService.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) { }
+
+  login(data: { identifier: string; password: string }) {
+    return this.http.post(`${this.apiUrl}/auth/login`, data).subscribe((resp: any) => {
+      localStorage.setItem('token', resp.token);
+    });
+  }
+}
--- src/app/features/auth/auth.ts
+++ src/app/features/auth/auth.ts
@@ -2,18 +2,39 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Footer } from '../../shared/components/footer/footer';
 import { Header } from '../../shared/components/header/header';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../core/services/authService';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-auth',
-  imports: [CommonModule, Footer, Header],
+  standalone: true,
+  imports: [CommonModule, FormsModule, Footer, Header, HttpClientModule],
   templateUrl: './auth.html',
   styleUrl: './auth.css',
 })
-
 export class Auth {
   activeTab: 'login' | 'register' = 'login';
 
   switchTab(tab: 'login' | 'register') {
     this.activeTab = tab;
   }
+
+  identifier = "";
+  password = "";
+
+  constructor(private authenticator: AuthService) {}
+
+  async onLogin() {
+  try {
+    const result = await this.authenticator.login({
+      identifier: this.identifier,
+      password: this.password
+    });
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 }
