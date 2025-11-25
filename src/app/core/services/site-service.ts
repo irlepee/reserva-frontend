d8fe5590d39b005a3ecf887e4ce60987e3069b8c
AlexIA075 <163032623+irlepee@users.noreply.github.com>
2025-11-25 00:36:08 -0700

    Todo funcionando, lo siguiente es dar estilos y colocar botones y demás

---
+++ src/app/core/services/site-service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SiteService {
+  constructor(private http: HttpClient) { }
+
+  api = 'http://localhost:3000';
+  
+  getSites(): Promise<any[]> {
+    return this.http
+      .get<any[]>(`${this.api}/sites`)
+      .toPromise()
+      .then(r => r ?? []);
+  }
+}
