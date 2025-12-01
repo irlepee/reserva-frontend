1db6caacedb64a5436b2be0cfb27305fa3e63883
AlexIA075 <163032623+irlepee@users.noreply.github.com>
2025-11-30 22:41:14 -0700

    Refactorización comenzada

---
+++ src/app/features/dashboard/groups/dash-group/dash-group.ts
@@ -0,0 +1,77 @@
+import { Component } from '@angular/core';
+import { GroupService } from '../../../../core/services/group-service';
+import { CommonModule } from '@angular/common';
+import { GroupFilter } from "../../../../shared/components/filters/group-filter/group-filter";
+import { AuthService } from '../../../../core/services/authService';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-dash-group',
+  imports: [CommonModule, GroupFilter, FormsModule],
+  templateUrl: './dash-group.html',
+  styleUrl: './dash-group.css',
+})
+export class DashGroup {
+  constructor(private groupService: GroupService, private authService: AuthService) { }
+
+  originalGroups: any = [];
+  groups: any = [];
+  mostrarFiltros = false;
+  user: any;
+  search: string = '';
+
+  toggleFiltros() {
+    this.mostrarFiltros = !this.mostrarFiltros;
+  }
+
+  ngOnInit() {
+    this.groupService.getGroups()
+      .then(data => {
+        this.groups = data;
+        this.originalGroups = data;
+        console.log(data);
+      })
+      .catch(err => {
+        console.log(err);
+        this.groups = [];
+      });
+    this.authService.fetchCurrentUser()
+      .then(userData => {
+        this.user = userData;
+        console.log('Usuario actual:', this.user);
+      })
+      .catch(err => {
+        console.log('Error al obtener el usuario actual:', err);
+      });
+  }
+
+  filtroActual: string = '';
+
+  aplicarFiltro(filtro: string) {
+    this.filtroActual = filtro;
+    if (filtro === 'all') {
+      this.groups = this.originalGroups;
+    }
+    if (filtro === 'created') {
+      this.groups = this.originalGroups.filter(
+        (group: any) => group.id_owner === this.user.id
+      );
+    }
+    if (filtro === 'others') {
+      this.groups = this.originalGroups.filter(
+        (group: any) => group.id_owner !== this.user.id
+      );
+    }
+
+  }
+
+  filtrarPorNombre() {
+    if (this.search.trim() === '') {
+      this.groups = this.originalGroups;
+    } else {
+      this.groups = this.originalGroups.filter(
+        (group: any) => group.name.toLowerCase().includes(this.search.toLowerCase())
+      );
+    }
+  }
+}
